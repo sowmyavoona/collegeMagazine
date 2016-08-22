eaee6cb85746d5430ed271089e1ff28bb42ef518
jesse-sravya <jesse.dhara@gmail.com>
2016-08-22 18:57:14 +0530

    The Project begins...
    - Made primary folder structure
    - Has bootstrap folder for offline works
    - Has layout borders to check the correctness of the layout

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
+
+</head>
+<body>
+
+<div class="container-fluid">
+
+	<!-- SUPER HEADER -->
+
+	<div class="row super-header text-center">
+		Funbee.
+	</div>
+
+	<!-- NAV BAR -->
+	<div class="row">
+	<div class="header">
+		<nav class="navbar navbar-bg-faded">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">WebSiteName</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#">Life</a></li>
+				<li><a href="#">College</a></li> 
+				<li><a href="#">Hacks</a></li> 
+			</ul>
+			<form class="form-inline pull-xs-right text-right">
+				<input class="form-control" type="text" placeholder="Search">
+				<button class="btn btn-outline-success" type="submit">Search</button>
+			</form>
+		</nav>
+	</div>
+		
+	</div>
+
+	<!-- WEB CONTENT -->
+	<div class="row">
+
+		<!-- Articles -->
+
+		<div class="col-xs-9">
+			<div class="articles row">
+				<div class="article-title col-xs-8">The Mage from Varrock</div>
+				<div class="article-date col-xs-4 text-right">Aug 22, 2016</div>
+				<div class="article-content col-xs-12">I've been killed more than fifty times by the wizards of Varrock. They're simply hard to defeat. I'm always at full health when ever I pass through these wild mages. I end up dying most of the times.</div>
+				<div class="article-fans col-xs-4"><3</div>
+				<div class="article-author col-xs-8 text-right">By RuneScaper</div>
+			</div>
+		</div>
+
+
+
+		<!-- Popular Feed -->
+
+		<div class="col-xs-3">
+		<div class="navbar-header">
+				<a class="navbar-brand" href="#">WebSiteName</a>
+			</div>
+			<ul class="nav text-right">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#">Life</a></li>
+				<li><a href="#">College</a></li> 
+				<li><a href="#">Hacks</a></li> 
+			</ul>
+
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
