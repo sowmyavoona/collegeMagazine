d200d1992a72b54c2daa1924e39497d14498af02
jesse-sravya <jesse.dhara@gmail.com>
2016-08-22 20:53:09 +0530

    Update phpVariables.php

--- phpVariables.php
+++ phpVariables.php
@@ -3,22 +3,22 @@
 	//$numberOfArticles = getNumberOfArticles();
 
 	global $titleArray;
-	global  $authorArray;
+	global $authorArray;
 	global $contentArray;
 	global $dateArray;
 	global $likesArray;
 
 	$query = "select * from article";
 	
-	if($query_run = mysqli_query($mysql_connect, $query)) {
+	if ($query_run = mysqli_query($mysql_connect, $query)) {
 		$query_run = mysqli_query($mysql_connect, $query);
 			
-		if(mysqli_num_rows($query_run)>0){
-			$i=0;
+		if (mysqli_num_rows($query_run) > 0){
+			$i = 0;
 			
-			while($query_row = mysqli_fetch_assoc($query_run) ){
+			while ($query_row = mysqli_fetch_assoc($query_run) ){
 				
-				if($query_row['isConfirmed'] == 1){
+				if ($query_row['isConfirmed'] == 1) {
 					$titleArray[$i] = $query_row['title'];
 					$authorArray[$i] = $query_row['author'];
 					$contentArray[$i] = $query_row['content'];
@@ -27,7 +27,7 @@
 					$i++;
 				}
 			}
-			$i=0;
+			$i = 0;
 		}
 	}	
 ?>
